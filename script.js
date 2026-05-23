5daf20742d965c9cb3b7520acd50e997a29d1144
Alhoniti Ibraheem <alhoniti.ibraheem@gmail.com>
2026-05-23 18:40:21 +0200

    adding total, drag+drop

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+let total = 0;
+
 function showOrganizing() {
     document.getElementById("organizingSection").style.display = "flex";
 }
 
-function addFlower(flowerImage) {
-    const bouquetArea = document.getElementById("bouquetArea");
+const bouquetArea = document.getElementById("bouquetArea");
+
+bouquetArea.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+bouquetArea.addEventListener("drop", function(event) {
+    event.preventDefault();
+
+    const flowerId = event.dataTransfer.getData("text");
+
+    const originalFlower = document.getElementById(flowerId);
 
     const newFlower = document.createElement("img");
-    newFlower.src = flowerImage;
+
+    newFlower.src = originalFlower.src;
     newFlower.className = "added-flower";
 
-    const randomX = 220 + Math.random() * 120;
-    const randomY = 170 + Math.random() * 120;
+    const rect = bouquetArea.getBoundingClientRect();
 
-    newFlower.style.left = randomX + "px";
-    newFlower.style.top = randomY + "px";
+    const x = event.clientX - rect.left - 40;
+    const y = event.clientY - rect.top - 40;
+
+    newFlower.style.left = x + "px";
+    newFlower.style.top = y + "px";
 
     bouquetArea.appendChild(newFlower);
+
+    total += 10;
+
+    document.getElementById("totalPrice").innerText = total;
+});
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
 }
