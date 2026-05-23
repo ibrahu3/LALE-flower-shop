b9afbc774d4f56e2a63f35e4a67104a524580ef2
Alhoniti Ibraheem <alhoniti.ibraheem@gmail.com>
2026-05-23 18:31:23 +0200

    adding the images + the thingy

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function organizingClicked() {
-    alert("Organizing button clicked!");
-    //this is the start
+function showOrganizing() {
+    document.getElementById("organizingSection").style.display = "flex";
+}
+
+function addFlower(flowerImage) {
+    const bouquetArea = document.getElementById("bouquetArea");
+
+    const newFlower = document.createElement("img");
+    newFlower.src = flowerImage;
+    newFlower.className = "added-flower";
+
+    const randomX = 220 + Math.random() * 120;
+    const randomY = 170 + Math.random() * 120;
+
+    newFlower.style.left = randomX + "px";
+    newFlower.style.top = randomY + "px";
+
+    bouquetArea.appendChild(newFlower);
 }
